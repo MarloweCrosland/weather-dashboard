fee9f90e9a7974b43851eac1bbad6b62ec42a8cb
marlowe <marlowepdx@gmail.com>
2022-06-13 13:04:12 -0700

    Pseudocode js functions

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//To-do list
+
+//searchCity()
+
+//displayWeather()
+//name, date, icon, temp, humidity, wind, uv
+//uv index change color
+
+//displayForecast()
+//5-day forecast date, icon, temp, wind, humididty
+
+//addHistory()
+//add search function to history items when clicked
